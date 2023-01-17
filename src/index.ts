3ffefff496c5ef2f549decd49df2ec40a3fde6ca
Iryna <iryna.markovich@vocovo.com>
2023-01-17 13:33:09 +0100

    add use of ws streams

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,35 @@
-import { WebSocketServer } from 'ws'
+import { WebSocketServer, createWebSocketStream } from 'ws'
 import commandPalette from './handlers/index'
 
 const PORT = 8080
+const options = { host: 'localhost', port: PORT }
 
-const wss = new WebSocketServer({ port: PORT }, () => {
-  console.log(`WebSocket Server started on ws://localhost:${PORT}`)
+const wss = new WebSocketServer(options, () => {
+  console.log(
+    `WebSocket Server started on ws://${options.host}:${options.port}`
+  )
+  console.log(options)
 })
 
 const runServer = () => {
   wss.on('connection', (ws) => {
     console.log(`Connected`)
 
-    ws.on('message', async (data) => {
+    const stream = createWebSocketStream(ws, {
+      encoding: 'utf8',
+      decodeStrings: false,
+    })
+
+    stream.on('data', async (data) => {
       const [command, ...options] = data.toString().split(' ')
 
       try {
         const cmd = commandPalette[command]
-        const result = await cmd(options.map((o) => parseInt(o, 10)))
+        const result = await cmd(options.map((o: string) => parseInt(o, 10)))
 
-        ws.send(`${command} ${result}`)
+        console.log('send: %s', command, result || '')
+
+        stream.write(`${command} ${result}`)
       } catch (e) {
         console.log(`Command not found: ${e}`)
       }
